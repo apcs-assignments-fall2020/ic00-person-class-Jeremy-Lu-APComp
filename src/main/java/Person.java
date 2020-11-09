ea817b45650980210f633e367eed8a3b55e5e8ff
Jeremy-Lu-APComp <21jlu@athenian.org>
2020-11-09 03:51:03 +0000

    Added code for Person class

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,40 @@
 public class Person {
+    private String first_name;
+    private String last_name;
     
+    public Person(String first_name, String last_name){
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+    public Person(){
+        this.first_name = "Jeremy";
+        this.last_name = "Lu";
+    }
+    public void doSomething(){
+        System.out.println("I am studying for a CompSci Test!");
+    }
+    public String toString(){
+        String str = "First name: " + first_name + ", Last name: " + last_name;
+        return str;
+    }
+    public String getFName(){
+        return this.first_name;
+    }
+    public String getLName(){
+        return this.last_name;
+    }
+    public void setFName(String first_name) {
+        // Don't allow blank names
+        if (first_name.trim().length() != 0) {
+            this.first_name = first_name;
+        }
+    }
+    public void setLName(String last_name) {
+        // Don't allow blank names
+        if (last_name.trim().length() != 0) {
+            this.last_name = last_name;
+        }
+    }
 }
 
 
--- src/main/java/PersonClient.java
+++ src/main/java/PersonClient.java
@@ -1,5 +1,11 @@
 public class PersonClient {
     public static void main(String[] args) {
-        
+        Person p1 = new Person("Jeremy", "Lu");
+        p1.setFName("Steve");
+        p1.setLName("Stevenson");
+        System.out.println(p1);
+        p1.doSomething(); //
+        Person p2 = new Person();
+        System.out.println(p2);
     }
 }
